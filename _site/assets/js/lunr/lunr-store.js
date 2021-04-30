e967de483c8cf2ceb7d2855cf8a638579270e4bb
김윤서(Yunseo Kim) <yunseo47@protonmail.ch>
2021-04-30 16:43:01 +0900

    jekyll-cache update

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "GitHub 마크다운 문법 정리",
+        "excerpt":"GitHub Pages 활용을 위해서는 markdown 문법에 대해 알 필요가 있다. GitHub 공식 문서의 Mastering Markdown과 Basic writing and formatting syntax를 참고하여 작성하였다. 1. 마크다운이란 마크다운(markdown)은 일반 텍스트 기반의 경량 마크업 언어다. 일반 텍스트로 서식이 있는 문서를 작성하는 데 사용되며, 일반 마크업 언어에 비해 문법이 쉽고 간단한 것이 특징이다. HTML과 리치...","categories": ["Jekyll"],
+        "tags": ["Jekyll"],
+        "url": "https://yunseo47.github.io//jekyll/GitHub-%EB%A7%88%ED%81%AC%EB%8B%A4%EC%9A%B4-%EB%AC%B8%EB%B2%95-%EC%A0%95%EB%A6%AC/",
+        "teaser": "https://yunseo47.github.io//assets/img/electronic.jpg"
+      },{
+        "title": "머신러닝 개발환경 구축하기",
+        "excerpt":"개요 이 글에서는 로컬 머신에서 머신러닝을 공부하기 위한 첫 단계라고 할 수 있는 개발환경 구축 방법에 대해 다룬다. 모든 내용은 우분투 20.04 LTS상에서 NVIDIA Geforce RTX 3070 그래픽카드를 기준으로 작성하였다. 구축할 기술 스택 Ubuntu 20.04 LTS Python 3.8 pip 21.0.1 jupyter matplotlib numpy pandas scipy scikit-learn CUDA 11.0.3 cuDNN 8.0.5...","categories": ["Data science"],
+        "tags": ["Machine learning","Deep learning"],
+        "url": "https://yunseo47.github.io//data%20science/%EB%A8%B8%EC%8B%A0%EB%9F%AC%EB%8B%9D-%EA%B0%9C%EB%B0%9C%ED%99%98%EA%B2%BD-%EA%B5%AC%EC%B6%95%ED%95%98%EA%B8%B0/",
+        "teaser": "https://yunseo47.github.io//assets/img/electronic.jpg"
+      },{
+        "title": "Qiskit Hackathon Korea 2021, 그 4일간의 기록",
+        "excerpt":"이 글의 목적 최근 2월 16일부터 2월 19일까지 4일간 진행된 Qiskit Hackathon Korea 2021에 “Qiskit과 PyTorch를 이용한 양자 하이브리드 신경망 구현”이라는 주제로 참여할 기회가 있었다. 시간이 오래 지나면 이번 해커톤에 대한 기억이 희미해질 것 같아, 좀 두서없는 글이라도 참가 동기부터 4일간의 활동, 그리고 해커톤이 끝난 후에 얻은 교훈과 느낀 점까지...","categories": ["Quantum Computing"],
+        "tags": ["Machine learning","Deep learning","Qiskit"],
+        "url": "https://yunseo47.github.io//quantum%20computing/Qiskit-Hackathon-Korea-2021,-%EA%B7%B8-4%EC%9D%BC%EA%B0%84%EC%9D%98-%EA%B8%B0%EB%A1%9D/",
+        "teaser": "https://yunseo47.github.io//assets/img/electronic.jpg"
+      },{
+        "title": "kaggle-Intro to Machine Learning 코스 내용 정리",
+        "excerpt":"최근 박해선 님께서 진행하시는 머신러닝 스터디 잼에 참가하게 되었다. Kaggle 공개 코스들을 이용하는 스터디 그룹 형식이고, 5개의 코스를 모두 수료하면 GDE(Google Developer Expert) 티셔츠도 받을 수 있다. 뭔가 Qiskit 해커톤을 시작으로 티셔츠 빌런이 되어가는 느낌이다. IBM에 이어 이번엔 GDE다. 각 코스를 수료할 때마다 해당 코스의 내용을 간단히 정리할 계획이다. 첫...","categories": ["Data science"],
+        "tags": ["Machine learning"],
+        "url": "https://yunseo47.github.io//data%20science/kaggle-Intro-to-Machine-Learning-%EC%BD%94%EC%8A%A4-%EB%82%B4%EC%9A%A9-%EC%A0%95%EB%A6%AC/",
+        "teaser": "https://yunseo47.github.io//assets/img/electronic.jpg"
+      }]
