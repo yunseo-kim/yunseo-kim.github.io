69b849f6175a2fdfbd254afa7cdfefbaf36f422a
김윤서(Yunseo Kim) <git@yunseo.kim>
2025-04-02 17:29:24 +0900

    Update prompt.py

--- tools/prompt.py
+++ tools/prompt.py
@@ -44,9 +44,12 @@ def translate(filepath, source_lang, target_lang):
         and data science for his Jekyll blog.</role> The customer's request is as follows:\n\n\
         <task>Please translate the provided <format>markdown</format> text from <lang>{source_lang}</lang> \
         to <lang>{target_lang}</lang> while preserving the format.</task> \
-        In the provided markdown format text, \n\
-        - <condition>please do not modify the YAML front matter except for the 'title' and 'description' tags, \
+        In the provided markdown format text: \n\
+        - <condition>Please do not modify the YAML front matter except for the 'title' and 'description' tags, \
         under any circumstances, regardless of the language you are translating to.</condition> \n\n\
+        - <condition>For the description tag, this is a meta tag that directly impacts SEO. \
+          Keep it broadly consistent with the original description tag content and body content, \
+          but adjust the character count appropriately considering SEO. \n\n\
         - <condition>The original text provided may contain parts written in languages other than {source_lang}. This is one of two cases. \n\
           1. The term may be a technical term used in a specific field with a specific meaning, so a standard English expression is written along with it. \n\
           2. it may be a proper noun such as a person's name or a place name. \n\
@@ -61,7 +64,8 @@ def translate(filepath, source_lang, target_lang):
             - <example>Both 'Le puits carré infini 1D' and 'Le puits carré infini 1D(The 1D Infinite Square Well)' are acceptable \
               French translations for '1차원 무한 사각 우물(The 1D Infinite Square Well)'. You can choose whichever you think is more appropriate.</example>\n\
           <else>In the second case, the original spelling of the proper noun in parentheses must be preserved in the translation output in some form.</else> \n\
-            - <example> '패러데이(Faraday)', '맥스웰(Maxwell)', '아인슈타인(Einstein)' should be translated into Japanese as 'ファラデー(Faraday)', 'マクスウェル(Maxwell)', and 'アインシュタイン(Einstein)'.\
+            - <example> '패러데이(Faraday)', '맥스웰(Maxwell)', '아인슈타인(Einstein)' should be translated into Japanese \
+              as 'ファラデー(Faraday)', 'マクスウェル(Maxwell)', and 'アインシュタイン(Einstein)'.\
               In languages ​​such as Spanish or Portuguese, they can be translated as 'Faraday', 'Maxwell', 'Einstein', in which case, \
               redundant expressions such as 'Faraday(Faraday)', 'Maxwell(Maxwell)', 'Einstein(Einstein)' would be highly inappropriate.</example>\
           </condition>\n\n\
@@ -72,7 +76,7 @@ def translate(filepath, source_lang, target_lang):
             (/posts/Neutron-Interactions-and-Cross-sections/#단면적cross-section-또는-미시적-단면적microscopic-cross-section)' \
             would be '[Neutronenwechselwirkungen und Wirkungsquerschnitte](/posts/Neutron-Interactions-and-Cross-sections/\
             #wirkungsquerschnitt-cross-section-oder-mikroskopischer-wirkungsquerschnitt-microscopic-cross-section)'.</example></condition> \n\n\
-        - <condition>Posts in this blog use the holocene calendar as the year numbering system, \
+        - <condition>Posts in this blog use the holocene calendar(인류력, 人類紀元) as the year numbering system, \
           so the year and date notations such as followings are intentional, not typos.\n\
           - <example>12024년 6월 21일</example>\n\
           - <example>12024.10.31.</example>\n\
