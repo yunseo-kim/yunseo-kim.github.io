a9475940080c8573d1d9c3f62365e7fa658b602e
Yunseo Kim <git@yunseo.kim>
2025-09-10 04:56:22 +0900

    chore(jampack): adjust features per stability
    - Disable inline_critical_css due to unstable style loading; should re-enable after fix
    - Disable video autoplay_lazyload (jampack-lozad class override bug upstream); keep off until upstream release includes fix
    - Enable image CDN optimization (process: optimize) to prep for future CDN adoption (e.g., Cloudflare Images); no effect for current jsDelivr

--- jampack.config.mjs
+++ jampack.config.mjs
@@ -3,7 +3,17 @@ export default {
     browserslist: '> 0.2%, last 2 versions, Firefox ESR, not dead and fully supports loading-lazy-attr', // defaults = '> 0.5%, last 2 versions, Firefox ESR, not dead'
   },
   css: {
-    inline_critical_css: true,
+    inline_critical_css: false,
+  },
+  image: {
+    cdn: {
+      process: 'optimize',
+    },
+  },
+  video: {
+    autoplay_lazyload: {
+      when: 'never',
+    }
   },
   misc: {
     prefetch_links: 'in-viewport',
