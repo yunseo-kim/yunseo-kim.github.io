ad69f54d91e150e1401f99ccdfe9c557e0022275
Yunseo Kim <git@yunseo.kim>
2025-09-11 01:28:26 +0900

    chore(jampack): disable CSS-related features for stability
    - Turn off inline_critical_css to avoid race between font definition load and inlined font usage
    - Temporary mitigation while investigating layout breakage due to fallback fonts being applied
    - Other non-CSS features remain unchanged

--- jampack.config.mjs
+++ jampack.config.mjs
@@ -3,7 +3,7 @@ export default {
     browserslist: '> 0.2%, last 2 versions, Firefox ESR, not dead and fully supports loading-lazy-attr', // defaults = '> 0.5%, last 2 versions, Firefox ESR, not dead'
   },
   css: {
-    inline_critical_css: true,
+    inline_critical_css: false,
   },
   image: {
     cdn: {
